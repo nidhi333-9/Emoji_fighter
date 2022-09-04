412b088521da87d01017965c62f4bd293681a993
Nidhi <84095928+nidhi333-9@users.noreply.github.com>
2022-09-04 19:00:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let stageEl = document.getElementById("stage");
 let fighterBtn = document.getElementById("fight-btn");
 function fightBtn() {
     for (let i = 0; i < fighters.length; i++) {
-        let randomEmojiOne = Math.floor(Math.random() * 118)
-        let randomEmojiTwo = Math.floor(Math.random() * 118)
+        let randomEmojiOne = Math.floor(Math.random() * fighters.length)
+        let randomEmojiTwo = Math.floor(Math.random() * fighters.length)
         stageEl.textContent = fighters[randomEmojiOne] + "Vs" + fighters[randomEmojiTwo]
     }
 }
